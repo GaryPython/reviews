dc2ede61f2826cc112ed7bab8f5799d792d4a52c
Gary Lai <a0983990529@gmail.com>
2020-01-08 16:17:30 +0800

    Advanced version

--- r2.py
+++ r2.py
@@ -10,17 +10,21 @@ for d in data:
 	sum_len = sum_len + len(d)
 print('每筆留言平均是', sum_len/len(data))
 
+good = [d for d in data if 'good' in d] 
+print(len(good))
+#new = []
+#for d in data:
+#	if len(d) < 100:
+#		new.append(d)
+#print('一共有', len(new), '筆留言資料小於100')
+#print(new[0])
 
-new = []
+good = [] 
 for d in data:
-	if len(d) < 100:
-		new.append(d)
-print('一共有', len(new), '筆留言資料小於100')
-print(new[0])
-
-good = []
-for d in data:
-	if 'good' in d :
+	if 'good' in d:
 		good.append(d)
 print('一共有', len(good), '筆留言提到good')
-print(good[0])
+#print(good[0])
+
+
+
