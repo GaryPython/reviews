4bb54fdc35f849bb67330c2c4605ff91052955f5
Gary Lai <a0983990529@gmail.com>
2020-01-08 15:39:20 +0800

    version two

---
+++ r2.py
@@ -0,0 +1,19 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+print('檔案讀取完成,總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('每筆留言平均是', sum_len/len(data))
+
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言資料小於100')
+print(new[0])
